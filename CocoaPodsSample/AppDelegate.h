582330bb96a3988affed7b63f312b77c7233edee
Kosuke Matsuda <matsuda@D1020209.local>
2011-11-18 17:51:20 +0900

    create project

---
+++ CocoaPodsSample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CocoaPodsSample
+//
+//  Created by Kosuke Matsuda on 11/11/18.
+//  Copyright (c) 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
